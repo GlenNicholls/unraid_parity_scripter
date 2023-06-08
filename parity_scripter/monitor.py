fedeb28b4984a00ae7b26cec2103baec0a49c6cf
GlenNicholls <dkillers303@gmail.com>
2023-06-08 14:30:46 -0600

    removed monitor file since it moved to unraid file

--- parity_scripter/monitor.py
+++
@@ -1,9 +0,0 @@
-"""Script to allow running scripts when parity checks are started/stopped."""
-
-import logging
-import pprint
-
-from dataclasses import dataclass
-from typing import Callable, Dict, List
-
-logger = logging.getLogger(__name__)
